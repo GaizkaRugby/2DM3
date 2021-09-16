340557ec1d19a0472929a7fc32255903c1c758e3
GaizkaRugby <90390482+GaizkaRugby@users.noreply.github.com>
2021-09-16 12:39:26 +0200

    Update Alumno.java

--- ejer2/src/ejer2/Alumno.java
+++ ejer2/src/ejer2/Alumno.java
@@ -10,10 +10,6 @@ public class Alumno extends Persona{
 	
 	public Alumno(String nombre, String apellido, String telefono, String DNI, int curso, double nota) {
 		super();
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.telefono = telefono;
-		this.DNI = DNI;
 		this.curso = curso;
 		this.nota = nota;
 	}
@@ -35,15 +31,6 @@ public class Alumno extends Persona{
 	public String toString() {
 		return "Alumno [nombre=" + nombre + ", apellido=" + apellido + ", telefono=" + telefono + ", DNI=" + DNI
 				+ ", curso=" + curso + ", nota=" + nota + "]";
-	}
-	
-	public void mostrarPersona() {
-		System.out.println("El nombre es: "+this.nombre+".");
-		System.out.println("El apellido es: "+this.apellido+".");
-		System.out.println("El telefono es: "+this.telefono+".");
-		System.out.println("El DNI es: "+this.DNI+".");
-		System.out.println("Esta en el curso "+this.curso+".");
-		System.out.println("Su nota es un "+this.nota+".");
 	}
 	
 	
